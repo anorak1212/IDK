0b1caad0793f8d2bd07980659207047185673b1f
anorak1212 <mateitojimezper.z212@gmail.com>
2025-01-10 01:21:39 -0600

    Subiendo página web

---
+++ escripts/massages.js
@@ -0,0 +1,100 @@
+// Fecha objetivo: 26 de junio de este año a las 11:11
+const targetDate = new Date(new Date().getFullYear(), 5, 26, 11, 11, 0);
+
+const textContainer = document.getElementById("text-container");
+
+const messages = [
+    "> Como humanos, la música nos transmite emociones",
+    "> y nos lleva a momentos de vida que quedan almacenados",
+    "> en nuestra memoria para siempre.",
+    ""
+];
+
+let index = 0;
+function updateCountdownMessage() {
+    const now = new Date();
+    const timeRemaining = targetDate - now;
+
+    if (timeRemaining <= 0) {
+        messages[3] = "> File accessed successfully";
+        printLink();
+        return;
+    }
+
+    const days = Math.floor(timeRemaining / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((timeRemaining / (1000 * 60 * 60)) % 24);
+    const minutes = Math.floor((timeRemaining / (1000 * 60)) % 60);
+    const seconds = Math.floor((timeRemaining / 1000) % 60);
+
+    messages[3] = `> Access in ${days} days, ${hours} hours, ${minutes} minutes y ${seconds} seconds.`;
+}
+
+function printNextMessage() {
+    if (index < messages.length) {
+        if (index === 3) {
+            const line = document.createElement("div");
+            line.classList.add("line");
+            line.textContent = messages[3];
+            textContainer.appendChild(line);
+
+            const interval = setInterval(() => {
+                updateCountdownMessage();
+                line.textContent = messages[3];
+                if (new Date() >= targetDate) {
+                    clearInterval(interval);
+                }
+            }, 500);
+
+            return;
+        }
+
+        const line = document.createElement("div");
+        line.classList.add("line");
+        line.textContent = messages[index];
+        textContainer.appendChild(line);
+
+        requestAnimationFrame(() => {
+            line.classList.add("typing");
+        });
+
+        line.addEventListener("animationend", () => {
+            line.classList.add("typing-complete");
+            index++;
+            printNextMessage();
+        });
+    }
+}
+
+function printLink() {
+    const link = document.createElement("a");
+    link.href = "https://open.spotify.com/playlist/0VWAGDWCLErw9a7ZrmBR1X?si=oatWEnuNSgeZD6XnLfPkZA";
+    link.textContent = "";
+    link.target = "_blank"; 
+    link.style.color = "#00ff00"; 
+    link.style.textShadow = "0 0 79px #00ff00, 0 0 80px #00ff00"; 
+    link.style.whiteSpace = "nowrap";
+    link.style.overflow = "hidden";
+    link.style.borderRight = "2px solid #00ff00";
+    link.style.display = "inline-block";
+    textContainer.appendChild(link);
+
+    const textToWrite =
+        "https://open.spotify.com/playlist/0VWAGDWCLErw9a7ZrmBR1X?si=oatWEnuNSgeZD6XnLfPkZA";
+    let charIndex = 0;
+
+    function typeLink() {
+        if (charIndex < textToWrite.length) {
+            link.textContent += textToWrite[charIndex];
+            charIndex++;
+            setTimeout(typeLink, 50);
+        } else {
+            link.style.borderRight = "none";
+        }
+    }
+
+    typeLink();
+}
+
+updateCountdownMessage();
+
+printNextMessage();
