a92b7ca24f527513c7e0cb3fe15f63e7909a66c8
anorak1212 <mateitojimezper.z212@gmail.com>
2025-02-19 23:09:15 -0600

    Celulares

--- escripts/massages.js
+++ escripts/massages.js
@@ -3,25 +3,9 @@ const targetDate = new Date(new Date().getFullYear(), 6, 26, 11, 11, 0);
 const textContainer = document.getElementById("text-container");
 
 const messages = [
-    "> Hay personas que son como los atardeceres,",
-    "> No importa cuántas veces los veas, siempre te sorprenden.",
-    "> Tú eres así, Nani.",
-    "> Un respiro, una luz suave, alguien que hace que todo sea más fácil.",
-    "> No siempre entiendo todo lo que dices, pero me gusta escucharte.",
-    "> Porque cuando hablas de lo que amas, brillas.",
-    "> Y me gusta ver a la gente brillar.",
-    "> Gracias por aguantarme, por entenderme (o al menos intentarlo),",
-    "> Por escuchar mis ideas raras y por nunca juzgarme.",
-    "> Por reírte conmigo, por ser tan tú, por estar.",
-    "> Eres intensa, inteligente, detallista, feliz y emotiva.",
-    "> Y eso te hace increíble.",
-    "> No sé qué venga después, pero prometo estar ahí cuando me necesites,",
-    "> En las buenas, en las malas y en las veces que solo necesites desahogarte.",
-    "> Espero que la vida nos deje seguir compartiendo momentos chidos,",
-    "> Y si un día tomamos caminos distintos, quiero que sepas algo.",
-    "> Te deseo lo mejor, que hagas lo que quieras, que seas feliz.",
-    "> Porque te lo mereces.",
-    "> Feliz 14: https://open.spotify.com/playlist/6of1JDFe5TiVUfh5OQNAUI?si=3VZwdr4vTWShrrpx_CH6nw",
+    "> Mientras uno está vivo",
+    "> Uno debe amar lo más que pueda",
+    "> https://maps.app.goo.gl/EmuGykronmMAk4YJ9",
     " ",
 ];
 
@@ -31,7 +15,7 @@ function updateCountdownMessage() {
     const timeRemaining = targetDate - now;
 
     if (timeRemaining <= 0) {
-        messages[19] = "> File accessed successfully";
+        messages[3] = "> File accessed successfully";
         printLink();
         return;
     }
@@ -41,20 +25,20 @@ function updateCountdownMessage() {
     const minutes = Math.floor((timeRemaining / (1000 * 60)) % 60);
     const seconds = Math.floor((timeRemaining / 1000) % 60);
 
-    messages[19] = `> Access in ${days} days, ${hours} hours, ${minutes} minutes y ${seconds} seconds.`;
+    messages[3] = `> Access in ${days} days, ${hours} hours, ${minutes} minutes y ${seconds} seconds.`;
 }
 
 function printNextMessage() {
     if (index < messages.length) {
-        if (index === 19) {
+        if (index === 3) {
             const line = document.createElement("div");
             line.classList.add("line");
-            line.textContent = messages[19];
+            line.textContent = messages[3];
             textContainer.appendChild(line);
 
             const interval = setInterval(() => {
                 updateCountdownMessage();
-                line.textContent = messages[19];
+                line.textContent = messages[3];
                 if (new Date() >= targetDate) {
                     clearInterval(interval);
                 }
