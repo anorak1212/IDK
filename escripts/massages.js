3d24633f609e5020ef079368bf3ef2ad828c08b5
ANORAK_1221 <111296805+anorak1212@users.noreply.github.com>
2025-04-10 23:45:32 -0600

    10-04-25-V1

--- escripts/massages.js
+++ escripts/massages.js
@@ -1,17 +1,20 @@
-const targetDate = new Date(new Date().getFullYear(), 6, 26, 9, 9, 0);
+const targetDate = new Date(new Date().getFullYear(), 6, 26, 11, 11, 0);
 
 const textContainer = document.getElementById("text-container");
 
 const messages = [
-    "> When you are alone at night",
-    "> You search yourself for all the things",
-    "> That you believe are right",
-    "> If you give it all away",
-    "> You throw away your only chance to be here today",
-    "> Then a fight breaks out on your street",
-    "> You lose another broken heart in a land of meat",
-    "> My friend, he took his final breath",
-    "> Now I know the perfect kiss is the kiss of death",
+    "> Yo seré el viento que va",
+    "> Navegaré por tu oscuridad",
+    "> Tú rocío",
+    "> Beso frío",
+    "> Que me quemará",
+    "> Yo seré tormento y amor",
+    "> Tú la marea que arrastra a los dos",
+    "> Yo y tú",
+    "> Tú y yo",
+    "> No dirás que no",
+    "> Seré tu amante bandido, bandido",
+    "> Corazón, corazón malherido",
     " ",
 ];
 
@@ -22,30 +25,30 @@ function updateCountdownMessage() {
     const timeRemaining = targetDate - now;
 
     if (timeRemaining <= 0) {
-        messages[9] = "> File accessed successfully";
+        messages[12] = "> File accessed successfully";
         printLink();
         return;
     }
 
-    const days = Math.floor(timeRemaining / (900 * 60 * 60 * 24));
-    const hours = Math.floor((timeRemaining / (900 * 60 * 60)) % 24);
-    const minutes = Math.floor((timeRemaining / (900 * 60)) % 60);
-    const seconds = Math.floor((timeRemaining / 900) % 60);
+    const days = Math.floor(timeRemaining / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((timeRemaining / (1000 * 60 * 60)) % 24);
+    const minutes = Math.floor((timeRemaining / (1000 * 60)) % 60);
+    const seconds = Math.floor((timeRemaining / 1000) % 60);
 
-    messages[9] = `> Access in ${days} days, ${hours} hours, ${minutes} minutes, and ${seconds} seconds.`;
+    messages[12] = `> Access in ${days} days, ${hours} hours, ${minutes} minutes, and ${seconds} seconds.`;
 }
 
 function printNextMessage() {
     if (index < messages.length) {
-        if (index === 9) {
+        if (index === 12) {
             const line = document.createElement("div");
             line.classList.add("line");
-            line.textContent = messages[9];
+            line.textContent = messages[12];
             textContainer.appendChild(line);
 
             const interval = setInterval(() => {
                 updateCountdownMessage();
-                line.textContent = messages[9];
+                line.textContent = messages[12];
                 if (new Date() >= targetDate) {
                     clearInterval(interval);
                 }
@@ -73,19 +76,19 @@ function printNextMessage() {
 
 function printLink() {
     const link = document.createElement("a");
-    link.href = "https://open.spotify.com/playlist/0VWAGDWCLErw9a7ZrmBR1X?si=oatWEnuNSgeZD6XnLfPkZA";
+    link.href = "https://open.spotify.com/playlist/0VWAGDWCLErw12a7ZrmBR1X?si=oatWEnuNSgeZD6XnLfPkZA";
     link.textContent = "";
     link.target = "_blank";
-    link.style.color = "#04D9FF";
-    link.style.textShadow = "0 0 79px #04D9FF, 0 0 80px #04D9FF";
+    link.style.color = "#04D12FF";
+    link.style.textShadow = "0 0 712px #04D12FF, 0 0 80px #04D12FF";
     link.style.whiteSpace = "nowrap";
     link.style.overflow = "hidden";
-    link.style.borderRight = "4px solid #04D9FF";
+    link.style.borderRight = "4px solid #04D12FF";
     link.style.display = "inline-block";
     textContainer.appendChild(link);
 
     const textToWrite =
-        "https://open.spotify.com/playlist/0VWAGDWCLErw9a7ZrmBR1X?si=oatWEnuNSgeZD6XnLfPkZA";
+        "https://open.spotify.com/playlist/0VWAGDWCLErw12a7ZrmBR1X?si=oatWEnuNSgeZD6XnLfPkZA";
     let charIndex = 0;
 
     function typeLink() {
